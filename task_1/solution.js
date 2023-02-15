cb467117d1ad5b68981e63dcd7158fdf5f57aecd
Трофимова Светлана <strofimova@fil-it.ru>
2023-02-15 12:11:44 +0300

    Update task 1

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,7 +3,9 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
+    let message = productName + ' за ' + productPrice + ' теперь в корзине';
 
+    console.log(message);
     // создайте переменную message
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
@@ -23,6 +25,9 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue;
+
+    newValue = oldValue + 1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -38,6 +43,11 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
+    let newSum;
+    let newSumText;
+
+    newSum = oldSum + difference;
+    newSumText = '${newSum} +  ₽'
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
