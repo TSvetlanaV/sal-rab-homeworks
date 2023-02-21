8ff149ff9569eb7dcbd54129d243e816cb508049
Трофимова Светлана <strofimova@fil-it.ru>
2023-02-21 14:37:27 +0300

    Update task 4

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,11 +2,24 @@
 
 function parseProducts(json) {
     // Аргументом функции является JSON
+    let data = JSON.parse(json);
+    let product = data.products;
 
+    return data.products;
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
+}
+
+function renderProductsCards(json) {
+    clearProducts();
+    let products = parseProducts(json);
+    let length = products.length;
+
+    for(let i=0; i<=length -1; i++){
+        addProduct(products[i]);
+    }
 }
 
 // Напишите функцию renderProductsCards(json)
