50661898de304c3a3fc4c98ddb9a74ebbbe10864
Трофимова Светлана <strofimova@fil-it.ru>
2023-02-22 15:52:52 +0300

    Update task final

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,16 +38,18 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i <= countOfGoods -1; i += 1) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    let client = `${name} ${phone}`;
+    let addressString = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
 
-    data.client = 'Иван';
+    data.client = client;
+    data.order.address = addressString;
+    data.order.sum = sum;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
